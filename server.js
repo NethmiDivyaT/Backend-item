7929306bc4510a3b097a8994c8c4d315346126fc
NETHMI <nethmitharushika1999@gmail.com>
2021-05-09 00:15:15 +0530

    updat

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
+
+
+
 //configure db
 const db = require("./config/db.config.js");
 const mongoose = require('mongoose');
@@ -46,7 +49,6 @@ mongoose.connect(db.url, {
         process.exit();
     });
 
-require('./Online_Shopping/routes/item.routes.js')(app);
 
 
 
